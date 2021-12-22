957020c696ed53a17f3e55106f3bf31dd9102682
Kenneth Rørvik <krorvik@gmail.com>
2021-12-22 22:02:10 +0100

    add RWR viewport setup to RWR if not in place

--- multi.sh
+++ multi.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Need to make sure this is right
+TARGET="/cygdrive/d/DCS World/Mods/aircraft/F-16C/Cockpit/Scripts/EWS/RWR/indicator/RWR_ALR56_init.lua"
+
+DATA1='dofile(LockOn_Options.common_script_path.."ViewportHandling.lua")'
+DATA2='try_find_assigned_viewport("F16_RWR")'
+
+grep -q "try_find_assigned_viewport" "$TARGET" || echo "$DATA1" >> $TARGET
+grep -q "F16_RWR" "$TARGET" || echo $DATA2 >> "$TARGET"
+
+
 PORTS=$(mode.com | grep "device COM" | tr -d ':' | awk '{print $4}'| tr -d 'COM' | xargs)
 
 for PORT in $PORTS; do
